1a2aa488358f0a172316ba3e656a016a2f231470
Beth Ann <herbivore137@gmail.com>
2019-09-27 22:42:01 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,17 @@
+class Song < ApplicationRecord
+    validates :title, presence: true, uniqueness: { scope: [:artist_name, :released] }
+    # validates :title, uniqueness: { scope: [:artist_name, :released] }
+    #   message: "cannot be repeated by the same artist in the same year" }
+
+    validates :released, inclusion: { in: [true, false] }
+    # validates :released, exclusion: { in: [nil] }
+    
+    # validates :release_year, presence: true, if: :released?
+
+    validates :release_year, numericality: {less_than_or_equal_to: Date.today.year} 
+
+    def released?
+        self.released
+    end 
+
+end
